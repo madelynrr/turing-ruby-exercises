b9b23618b57140cb5dfcd2a0027925e8c69289a6
Madelyn Romero <m.romero013@gmail.com>
2020-06-05 15:26:40 -0600

    Set all attributes to incoming parameters

--- mythical-creatures/lib/dragon.rb
+++ mythical-creatures/lib/dragon.rb
@@ -3,9 +3,9 @@ class Dragon
   attr_reader :name, :color, :rider
 
   def initialize(name_attr, color_attr, rider_attr)
-    @name = "Ramoth"
-    @color = :gold
-    @rider = "Lessa"
+    @name = name_attr
+    @color = color_attr
+    @rider = rider_attr
   end
 
 end
