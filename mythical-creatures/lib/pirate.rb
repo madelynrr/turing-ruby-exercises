63346ffcb0606af71577ddfcffe3f8114f255f9f
Madelyn Romero <m.romero013@gmail.com>
2020-06-14 15:19:00 -0600

    Add heinous acts attribute set to 0

--- mythical-creatures/lib/pirate.rb
+++ mythical-creatures/lib/pirate.rb
@@ -5,13 +5,14 @@ class Pirate
   def initialize(name_param, job_param = "Scallywag")
     @name = name_param
     @job = job_param
+    @heinous_acts = 0
   end
 
   def cursed?
-
+    # true if @heinous_acts >= 3
   end
 
   def commit_heinous_act
-    
+    # @heinous_acts += 1
   end
 end
