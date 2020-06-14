64bad9fe76272a6cae3e320ae507921b8f27c533
Madelyn Romero <m.romero013@gmail.com>
2020-06-14 14:36:48 -0600

    Set incoming disposition parameter to default to homebody

--- mythical-creatures/lib/hobbit.rb
+++ mythical-creatures/lib/hobbit.rb
@@ -2,7 +2,7 @@ class Hobbit
 
   attr_reader :name, :disposition
 
-  def initialize(name_param)
+  def initialize(name_param, disposition_param = "homebody")
     @name = name_param
     @disposition = "homebody"
   end
